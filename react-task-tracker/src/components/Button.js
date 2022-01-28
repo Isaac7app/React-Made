1cb0ae6474cfee28a3aac6745ab88de6bfd79c43
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-28 16:35:10 +0100

    button working

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,5 +1,22 @@
-const Button = () => {
-  return  <button className='btn'>Add</button>
+const Button = ({color,text}) => {
+  
+  const onClick = () =>{
+      console.log('click');
+  }  
+
+
+  return ( 
+  <button
+  onClick={onClick} 
+  style={{ backgroundColor: color }} 
+   className='btn'>
+   {text}
+  </button>
+  )
+}
+
+Button.defaultProps = {
+    color: 'stealblue',
 }
 
 export default Button;
