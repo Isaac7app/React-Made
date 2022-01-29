c18da33ea16f8262b5acf3e7a27c5ad122c72a04
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-29 10:22:36 +0100

    writing toggle

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -31,11 +31,17 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle Reminder
+const toggleReminder = (id) =>{
+  console.log(id)
+
+}
+
   return (
     <div className="container">
      <Header />
      {tasks.length > 0 ?<Task tasks={tasks} 
-     onDelete={deleteTask}
+     onDelete={deleteTask} onToggle={toggleReminder}
      /> : 'No Tasks To Show'}
     </div>
   );
