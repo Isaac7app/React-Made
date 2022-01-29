4c837b7d5a715b8bc8e139bf82ce38f2f6e96ceb
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-29 09:35:03 +0100

    made the object global

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,11 +1,35 @@
 import Header from "./components/Header";
-import Task from "./components/Task";
+import { useState } from "react";
+import Task from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th AT 2:30pm',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: 'Meeting at school',
+        day: 'Feb 6th AT 11:30pm',
+        reminder: true,
+    },
+    {
+        id:3,
+        text: 'Food shoping',
+        day: 'Feb 5th AT 2:30pm',
+        reminder: false,
+    }
+
+
+])
+
   return (
     <div className="container">
      <Header />
-     <Task />
+     <Task tasks={tasks}/>
     </div>
   );
 }
