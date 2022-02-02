5d906059cc3d26028c13e7dcf197ee8381027a4e
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-02-02 11:34:43 +0100

    app working as a fullstack app

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -72,10 +72,20 @@ const toggleReminder = async (id) =>{
   const updTask = { ...taskToToggle,
   reminder: !taskToToggle.reminder}
 
+  const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    method: 'PUT',
+    headers:{
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
   setTasks(
     tasks.map((task)=> 
       task.id === id ? {...task, reminder:
-      !task.reminder} : task
+      data.reminder} : task
       )
       )
 
