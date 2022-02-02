906ba768525da2038f6d48f6f31da4c0eec66fa1
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-02-02 11:12:28 +0100

    changes

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -37,6 +37,10 @@ const addTask = async (task) => {
     body: JSON.stringify(task)
   })
 
+  const data = res.json()
+
+  setTasks([...tasks, data])
+
 
   // const id = Math.floor(Math.random() * 10000) + 1
   // const newTask = {id, ...task}
