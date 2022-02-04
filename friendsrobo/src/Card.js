257a9edbda8886c30e6110c51f352c77febd3446
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-02-04 19:40:17 +0100

    card component done

--- friendsrobo/src/Card.js
+++ friendsrobo/src/Card.js
@@ -1,7 +1,7 @@
 const Card = ({id, name, email}) => {
   return (
-  <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robots' src='https://robohash.org/test?size=200x200' />
+  <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
           <h2>{name}</h2>
           <p>{email}</p>
