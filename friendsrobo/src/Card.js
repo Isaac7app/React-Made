bdbe46294d81309ddc268b2e7394f68075002bd5
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-02-04 18:07:23 +0100

    added props

--- friendsrobo/src/Card.js
+++ friendsrobo/src/Card.js
@@ -1,10 +1,10 @@
-const Card = () => {
+const Card = ({id, name, email}) => {
   return (
   <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='robots' src='https://robohash.org/test?size=200x200' />
       <div>
-          <h2>Jane Doe</h2>
-          <p>jane.doe@gmail.com</p>
+          <h2>{name}</h2>
+          <p>{email}</p>
       </div>
       
   </div>
