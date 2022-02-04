8d52c275e6a780472e52c92f6328396593913ec2
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-02-04 17:19:29 +0100

    making the cards for the app

--- friendsrobo/src/index.js
+++ friendsrobo/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Hello />
-  </React.StrictMode>,
+   <Card />,
+  
   document.getElementById('root')
 );
 
