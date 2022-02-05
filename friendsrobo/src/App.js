59d8db0d323b3f881c1937fc256fefc42d909266
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-02-05 17:57:28 +0100

    state dynamnic

--- friendsrobo/src/App.js
+++ friendsrobo/src/App.js
@@ -3,21 +3,27 @@ import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 
-class App extends Component(){
+class App extends Component{
     constructor(){
         super()
         this.state = {
             robots : robots,
-            searchfiles: ''
+            searchfield: ''
         }
         
     }
+
+   onSearchChange (event){
+        console.log(event.target.value)
+    }
+
+
     render () {
         return (
             <div className="tc">
                 <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={robots}/>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={this.state.robots}/>
             </div>)
     }
   
