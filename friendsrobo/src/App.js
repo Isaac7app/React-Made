717a37c7db54c0065c5f5c7288d30cbf4f763b3c
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-02-05 17:05:42 +0100

    app component made

---
+++ friendsrobo/src/App.js
@@ -0,0 +1,11 @@
+import CardList from "./CardList";
+import { robots } from "./robots";
+
+const App = () => {
+  return (
+  <div>
+      <CardList robots={robots}/>
+  </div>)
+};
+
+export default App;
